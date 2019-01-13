d08e80712047fff65e9078ec0b6716270384b093
Sergey Zhitkevich <horrorerror92@gmail.com>
2019-01-13 16:07:41 +0300

    created layout and hoc in localstorage

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -15,20 +15,53 @@ class Todo extends PureComponent {
     return biggest + 1;
   }
 
-  handleChange = event => {};
+  handleChange = event => {
+    this.setState({
+    inputValue: event.target.value
+    });
+  };
 
-  createNewRecordByEnter = event => {};
-
+  createNewRecordByEnter = event => {
+    if(event.key === 'Enter') {
+      this.createNewRecord();
+    }
+  };
+    
   toggleRecordComplete = event => {};
 
   createNewRecord = () => {};
 
   render() {
-    return;
+
+    const { savedData } = this.props;
+    return (
+      <Card title = {`Список дел`}>
+      <div className = {`todo t-todo-list`}>
+        {this.renderEmptyRecord()}
+        {savedData.map(this.renderRecord)}
+      </div>
+    </Card>
+    );
   }
 
   renderEmptyRecord() {
-    return;
+    return (
+      <div className = {`todo-item todo-item-new`}>
+          <input 
+            className = {`todo-input t-input`} 
+            placeholder =  {`Введите задачу`} 
+            value = {this.state.inputValue}
+            onChange = {this.handleChange}
+            onKeyPress = {this.createNewRecordByEnter}
+          />
+          <span 
+            className = {`plus t-plus`} 
+            onClick={this.createNewRecord}
+          >
+            +
+          </span>
+        </div>    
+    );
   }
 
   renderRecord = record => {
