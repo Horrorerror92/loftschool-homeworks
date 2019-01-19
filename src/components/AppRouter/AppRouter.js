9a8587bdad4f6a831fa8041fe146d4c7fb702b78
Sergey Zhitkevich <horrorerror92@gmail.com>
2019-01-19 18:02:12 +0300

    App almost done

--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -18,13 +18,35 @@ import InboxMail from '../InboxMail';
 import OutboxList from '../OutboxList';
 import OutboxMail from '../OutboxMail';
 import style from './AppRouter.module.css';
+import classnames from 'classnames';
 
 class AppRouter extends Component {
   render() {
     return (
       <div className = {style.wrapper}>
         <div className = {style.container}>
-          
+          <nav className = {style.nav}>
+            <ul className = {classnames(style.navList, 't-nav-list')}>
+              <li className = {style.navElement}>
+                <Link to="/app" className={classnames(style.link, 't-link-home')}>
+                    Home
+                </Link>
+              </li>
+              <li className = {style.navElement}>
+                <Link to="/app/inbox" className={classnames(style.link, 't-link-inbox')}>
+                    Inbox
+                </Link>
+              </li>
+              <li className = {style.navElement}>
+                <Link to="/app/outbox" className={classnames(style.link, 't-link-outbox')}>
+                    Outbox
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <div className = {style.content}>
+            <h3 className = {style.title}>Home</h3>   
+          </div>
         </div>
       </div>
     )
