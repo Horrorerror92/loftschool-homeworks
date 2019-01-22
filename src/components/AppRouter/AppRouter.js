0a70200c5879b3db9e84a6fdda8c253c6b07d3ae
Sergey Zhitkevich <horrorerror92@gmail.com>
2019-01-22 19:08:18 +0300

    AppRouter, Serach 1/2 and actions

--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -1,3 +1,24 @@
 // Реализуйте роутер
 // Вам нужно определить корневой роут, который будет вести на страницу поиска.
 // Роут шоу должен принимать id в параметрах.
+
+import React, { Component } from 'react';
+import './AppRouter.css';
+import { Switch, Route } from 'react-router-dom';
+import Search from '../Search';
+import ShowPage from "../ShowPage";
+
+class AppRouter extends Component {
+  render() {
+    return ( 
+      <div className = {`App`}>
+        <Switch>
+          <Route exact path = '/' component = { Search } />
+          <Route path = 'shows/:id' component = { ShowPage }/>
+        </Switch>
+      </div>
+    )
+  }
+} 
+
+export default AppRouter;
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,3 +3,57 @@
 // Используйте метод connect и mapStateToProps, mapDispatchToProps,
 // чтобы получить ссылку на поле search вашего стейта
 // и экшн searchRequest.
+
+import React, { Component } from 'react';
+import style from './Search.module.css';
+import { searchRequest } from  '../../actions';
+
+
+class Search extends Component {
+
+  state = {
+    searchValue: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      searchValue: e.target.value
+    });
+  }
+
+  handleClick = (e) => {
+    e.preventdefault();
+    const { searchValue } = this.state;
+    const { searchRequest } = this.props;
+    
+    searchRequest(searchValue);
+
+    this.setState({
+      searchValue: ''
+    })
+
+  }
+
+  render(){
+    
+    const searchValue = this.state;
+
+    return (
+      <>
+        <div className = { style.previewList}>
+          <input
+          className = {style.input}
+          onChange = {this.handleChange}
+          placeholder = 'Название сериала'
+          value = { searchValue } 
+          />
+        </div>
+        <div className = {style.buttonWrapper}>
+          <button className = {style.button} onClick = {this.handleClick}>Найти</button>
+        </div>
+      </>
+    )
+  } 
+}
+
+export default Search;
